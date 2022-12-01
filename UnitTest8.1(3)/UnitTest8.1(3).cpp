32aeabc10ffc83f61adacdfd244383b67fe70fc4
yrii <yural1rn@gmail.com>
2022-12-01 21:14:16 +0200

    laba8.1(3)

---
+++ UnitTest8.1(3)/UnitTest8.1(3).cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba8.1(3)/laba8.1(3).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest813
+{
+	TEST_CLASS(UnitTest813)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char* str = "fff";
+			Count(str);
+			int expected = 2, actual;
+			Assert::AreEqual(expected, actual);
+		}
+	};
+}
